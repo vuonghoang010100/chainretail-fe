3026c803039055df472fc301058d4a5173dc3229
Vuong Hoang <vuonghoang010100@gmail.com>
2024-08-17 16:23:54 +0700

    feat: add Vendor View page with Contract details refer to issue #6

--- src/pages/tenant/Partner/Vendor/ViewVendor.jsx
+++ src/pages/tenant/Partner/Vendor/ViewVendor.jsx
@@ -8,8 +8,9 @@ import {
   Space,
   Button,
   Popconfirm,
+  Table,
 } from "antd";
-import { HomeOutlined } from "@ant-design/icons";
+import { HomeOutlined, PaperClipOutlined } from "@ant-design/icons";
 import { PageContent, PageHeader } from "@/components/layout/PageContent";
 import { ROUTE } from "@/constants/AppConstant";
 import { deleteRecord } from "./Vendor";
@@ -36,12 +37,29 @@ const breadcrumbItems = [
 
 const { Text } = Typography;
 
+const tabList = [
+  {
+    key: 1,
+    tab: "Thông tin nhà cung cấp",
+  },
+  {
+    key: 2,
+    tab: "Danh sách hợp đồng",
+  },
+];
+
 const ViewVendor = () => {
   const navigate = useNavigate();
   // -------------------- Page attr --------------------
   const { id } = useParams(); // id
   const [loading, setLoading] = useState(true);
   const [currentRecord, setCurrentRecord] = useState({}); // data
+
+  const [activeTabKey, setActiveTabKey] = useState(1);
+
+  const onTabChange = (key) => {
+    setActiveTabKey(key);
+  };
 
   // -------------------- Fetch data --------------------
   useEffect(() => {
@@ -126,6 +144,90 @@ const ViewVendor = () => {
     navigate(path);
   };
 
+  const colums = [
+    {
+      title: "Mã hợp đồng",
+      key: "id",
+      render: (_, record) => {
+        return (
+          <Link
+            to={`${ROUTE.TENANT_APP.CONTRACT.path}/${record.id}`}
+            target="_blank"
+          >
+            {record.id}
+          </Link>
+        );
+      },
+    },
+    {
+      title: "Ngày bắt đầu",
+      dataIndex: "startDate",
+      key: "startDate",
+    },
+    {
+      title: "Ngày kết thúc",
+      dataIndex: "endDate",
+      key: "endDate",
+    },
+    {
+      title: "Chu kỳ",
+      key: "period",
+      render: (_, record) => {
+        return record?.period + " Ngày";
+      },
+    },
+    {
+      title: "Nhập gần nhất",
+      dataIndex: "latestPurchaseDate",
+      key: "latestPurchaseDate",
+    },
+    {
+      title: "Hạn nhập hàng kế tiếp",
+      dataIndex: "nextPurchaseDate",
+      key: "nextPurchaseDate",
+    },
+    {
+      title: "Trạng thái",
+      dataIndex: "status",
+      key: "status",
+    },
+    {
+      title: "Tệp tin",
+      key: "pdfUrl",
+      render: (_, record) => (
+        <Link to={`${record?.pdfUrl}`} target="_blank">
+          {record?.pdfUrl && (
+            <>
+              <PaperClipOutlined /> Hợp đồng
+            </>
+          )}
+        </Link>
+      ),
+    },
+  ];
+
+  const contentList = {
+    1: (
+      <Descriptions
+        bordered
+        items={infoItems}
+        size="small"
+        column={1}
+        labelStyle={{
+          width: "30%",
+          minWidth: "max-content",
+        }}
+      />
+    ),
+    2: (
+      <Table
+        dataSource={currentRecord.contracts}
+        columns={colums}
+        pagination={false}
+      />
+    ),
+  };
+
   return (
     <PageContent>
       <PageHeader
@@ -150,17 +252,15 @@ const ViewVendor = () => {
           <Button onClick={() => navigate(path)}>Đóng</Button>
         </Space>
       </PageHeader>
-      <Card title="Thông tin nhà cung cấp" bordered={false} loading={loading}>
-        <Descriptions
-          bordered
-          items={infoItems}
-          size="small"
-          column={1}
-          labelStyle={{
-            width: "30%",
-            minWidth: "max-content",
-          }}
-        />
+      <Card
+        // title="Thông tin nhà cung cấp"
+        tabList={tabList}
+        activeTabKey={activeTabKey}
+        onTabChange={onTabChange}
+        bordered={false}
+        loading={loading}
+      >
+        {contentList[activeTabKey]}
       </Card>
       {/* TODO: contract list */}
     </PageContent>
